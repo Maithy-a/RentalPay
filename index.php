e9e0fa6175c2e166d17953b312ed94863c57ca56
Maithya <maithyabon2@gmail.com>
2024-10-21 14:16:41 +0300

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,6 @@ include 'conn.php';
    <link rel="icon" href="res/img/office.png">
   <link rel="stylesheet" href="res/css/index.css">
   <link rel="stylesheet" href="res/css/loader.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.5.0/mdb.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
